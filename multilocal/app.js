c632c6c1da4fd6c677e6cdb68b222dfe169d015c
olexanders17 <olexanders17@gmail.com>
2016-08-16 22:10:17 +0300

    some changes

--- multilocal/app.js
+++ multilocal/app.js
@@ -1,35 +1,78 @@
-var applyLanguage = function (lang) {
-    alert('now language is: ' + lang);
-}
+var lang;
+lang = 'ua';
 
-var getCurrentLanguage = function () {
-    var defaultLanguage = 'ua';
-    //
 
-    var lang = document.querySelector('input[type="radio"][name="lang"][checked]').value;
-
-    if (lang) {
-        defaultLanguage = lang;
+function clacLanguage() {
+    var rawLang = location.hash.slice(1);
+    if (rawLang) {
+        lang = rawLang;
     }
-    console.log(lang);
-    return defaultLanguage;
-}
-
-var currentLang = getCurrentLanguage();
-var langEls = document.getElementsByClassName('lang-' + currentLang);
-
-for (var i = 0; i < langEls.length; i++) {
-    var langEl = langEls[i];
-
-    // langEl.style.display = 'inline';
-    langEl.classList.add('visible');
+    var radio = document.querySelector('input[type="radio"][value=' + lang + "]");
+    if (radio) radio.setAttribute('checked', 'true');
+    location.hash = radio.getAttribute('value');
 }
 
 
-// $<prefix> == DOMElement
-var $save = document.querySelector('html body button#save')
+//
+//defaul action
+//
+
+clacLanguage();
+var checkedRadio;
+var $save = document.querySelector('html body button#save');
 
 $save.addEventListener('click', function () {
 
-    location.hash=currentLang;
+
+    checkedRadio = document.querySelector('input[type="radio"][name="lang"][checked="true"]');
+    console.log('clicked  ' + lang);
+
+    location.hash=checkedRadio.getAttribute('value');
+    console.log("location.hash: " + location.hash);
+    //location.hash = attribute;
+
 });
+
+
+/*
+ var applyLanguage = function (lang) {
+ alert('now language is: ' + lang);
+ }
+
+ var getCurrentLanguage = function () {
+
+ var defaultLanguage = 'ua';
+ var rawLang = location.hash.slice(1);
+
+ if (rawLang) {
+ defaultLanguage = rawLang;
+ document.querySelector('input[type="radio"][value=' + defaultLanguage + "]").setAttribute('checked','true' );
+ }
+
+ var lang = document.querySelector('input[type="radio"][name="lang"][checked]');
+
+ if (lang) {
+ defaultLanguage = lang.value;
+ }
+ console.log(lang);
+ return defaultLanguage;
+ }
+
+ var currentLang = getCurrentLanguage();
+ var langEls = document.getElementsByClassName('lang-' + currentLang);
+
+ for (var i = 0; i < langEls.length; i++) {
+ var langEl = langEls[i];
+
+ // langEl.style.display = 'inline';
+ langEl.classList.add('visible');
+ }
+ */
+
+
+// // $<prefix> == DOMElement
+// var $save = document.querySelector('html body button#save')
+//
+// $save.addEventListener('click', function () {
+//     location.hash = currentLang;
+// });
