58375c5927463fc3ae7e046c26b6e7f5ea600301
olexanders <aaa@ss.ty>
2016-08-16 12:28:40 +0000

    add index

--- message/app.js
+++ message/app.js
@@ -6,14 +6,16 @@ var navigatorMessageMap = {
 
 var platform = navigator.platform.toLowerCase();
 
-
+var isexistsOS = false;
 for (var key in navigatorMessageMap) {
     key = key.toLowerCase();
     var version = navigatorMessageMap[key];
-    console.log(navigatorMessageMap);
 
     if (platform.indexOf(key) >= 0) {
         document.querySelector('.download-message[data-version=' + version + ']').style.display = "block";
+        isexistsOS = true;
     }
-
+}
+if (!isexistsOS) {
+    document.querySelector('.download-message[data-version=' + "noname" + ']').style.display = "block";
 }
